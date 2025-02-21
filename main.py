909b224bbdbd20fa2591ba4982f0eb601c0538e8
Deeparth Gupta <dprthgpta@gmail.com>
2025-02-21 07:48:14 +0530

    Added name to ID map

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Material import Material
 
 material_totals = dict(None)
 
+
 def create_material_from_definition(material_definition):
     # Creates a material object from given material dictionary
 
@@ -28,11 +29,11 @@ def create_materials_from_file(file_name):
 
 
 def add_to_quantity(name, quantity):
-# Adds a key value pair if the key doesn't exist and adds to the existing value if it does
+    # Adds a key value pair if the key doesn't exist and adds to the existing value if it does
 
     if name not in material_totals.keys():
         material_totals[name] = quantity
-    
+
     else:
         existing_quantity = material_totals[name]
         material_totals[name] = existing_quantity + quantity
@@ -44,17 +45,17 @@ def subtract_from_quantity(name, quantity):
 
         if new_quantity < 0:
             remove_material(name)
-        
+
         else:
             material_totals[name] = new_quantity
 
 
 def remove_material(name):
-# Removes a key value pair from material_totals if it exists
+    # Removes a key value pair from material_totals if it exists
 
     if name in material_totals:
         material_totals.pop(name)
 
+
 def main():
     pi_materials = create_materials_from_file(None)
-    
