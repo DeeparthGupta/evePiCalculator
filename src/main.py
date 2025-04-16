5bda6eaea4faa8583f7066bc2755519586e4dc4b
Deeparth Gupta <dprthgpta@gmail.com>
2025-04-16 14:21:10 +0530

    Implemented commandline params

--- src/main.py
+++ src/main.py
@@ -3,7 +3,10 @@ import json
 import sys
 from collections import defaultdict
 
-from material_ops import calculate_material_requirements, create_master_data, material_id_to_name
+from material_ops import (
+    calculate_material_requirements,
+    create_master_data,
+)
 from utils import dict_binary_operation, dict_from_file
 
 
@@ -39,64 +42,79 @@ def parse_arguments():
     return arg_parser.parse_args()
 
 
-def main():
+def main() -> None:
     print("Creating material data.")
     try:
         pi_materials = dict_from_file("./data/pi_materials.json")
         if not pi_materials:
             raise ValueError("Input data is empty.")
+
     except Exception as error:
         print(f"Unable to load Material data: {error}")
         print("Exiting...")
         sys.exit(1)
+
     else:
         master_data = create_master_data(pi_materials)
-        name_id_map = dict_from_file("/data/name_id_map.json")
-        id_name_map = dict_from_file("/data/id_name_map.json")
 
-    args = parse_arguments()
+        args = parse_arguments()
 
-    if args.file:
-        try:
-            data = dict_from_file(args.file)
-        except Exception as error:
-            print(f"An Error has occured: {error}")
-
-    else:
-        try:
-            data = json.loads(args.input)
-
-            if not isinstance(data, dict):
-                raise TypeError(f"Expected a dictionary, got {type(data).__name__}")
-
-        except json.JSONDecodeError:
-            print("Malformed JSON")
-        except TypeError as error:
-            print(f"Invalid data format: {error}")
-        except Exception as error:
-            print(f"Unexpected Error occurred: {error}")
+        if args.file:
+            try:
+                data = dict_from_file(args.file)
+            except Exception as error:
+                print(f"An Error has occured: {error}")
+                sys.exit(1)
 
         else:
-            output = defaultdict(int)
-            for material_id, quantity in data.items():
-                material_requirements = calculate_material_requirements(
-                    material_id, quantity, master_data
-                )
+            try:
+                data = json.loads(args.input)
 
-                output = dict_binary_operation("add", output, material_requirements)
+                if not isinstance(data, dict):
+                    raise TypeError(f"Expected a dictionary, got {type(data).__name__}")
 
-            if args.named_out:
-                output = material_id_to_name(output, name_id_map)
+            except json.JSONDecodeError:
+                print("Malformed JSON")
+                sys.exit(1)
+            except TypeError as error:
+                print(f"Invalid data format: {error}")
+                sys.exit(1)
+            except Exception as error:
+                print(f"Unexpected Error occurred: {error}")
+                sys.exit(1)
 
-            if args.save:
-                try:
-                    print("Saving output to file")
-                    with open(args.save, "w") as outfile:
-                        json.dump(output, outfile)
-                except Exception as error:
-                    print(f"Error saving file: {error}")
+        if args.named_in:
+            try:
+                name_id_map = dict_from_file("/data/name_id_map.json")
+            except Exception as error:
+                print(f"Unable to process material names: {error}")
+            else:
+                data = {name_id_map[k]: v for k, v in data}
 
-            print(json.dumps(output, indent=2))
+        output = defaultdict(int)
+        for material_id, quantity in data.items():
+            material_requirements = calculate_material_requirements(
+                material_id, quantity, master_data
+            )
+            output = dict_binary_operation("add", output, material_requirements)
+
+        if args.named_out:
+            try:
+                id_name_map = dict_from_file("/data/id_name_map.json")
+            except Exception as error:
+                print(f"Unable to create material names: {error}")
+            else:
+                output = {id_name_map[k]: v for k, v in output}
+
+        if args.save:
+            try:
+                print("Saving output to file")
+                with open(args.save, "w") as outfile:
+                    json.dump(output, outfile, indent=2)
+            except Exception as error:
+                print(f"Error saving file: {error}")
+
+        print(json.dumps(output, indent=2))
 
 
 if __name__ == "__main__":
